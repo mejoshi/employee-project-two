9ad6f6ca85f00337f577ec3d39d8411968d7b8cf
Ashutosh Joshi <aarmxd@gmail.com>
2022-03-04 01:51:13 +0530

    Second Commit

---
+++ src/main/java/com/employeeProject/spring/employeeprojecttwo/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.employeeProject.spring.employeeprojecttwo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.employeeProject.spring.employeeprojecttwo.entity.Employee;
+import com.employeeProject.spring.employeeprojecttwo.repository.EmployeeRepository;
+
+
+@Controller
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	@GetMapping("/insert")
+	public String addEmployee(Model model) {
+		model.addAttribute("insert", new Employee(101,"gauri","ceo",420,19));
+		return "createEmployee";
+	}
+	
+	@PostMapping("/insert")
+	public String addEmployee(@ModelAttribute Employee employee, Model model) {
+		model.addAttribute("insert", employee);
+		return "result";
+	}
+	
+	@GetMapping("/update")
+	public String updateEmployee() {
+		return "UpdateEmployee";
+	}
+	
+	@GetMapping("/remove")
+	public String removeEmployee() {
+		return "DeleteEmployee";
+	}
+	
+}
--- src/main/java/com/employeeProject/spring/employeeprojecttwo/entity/Employee.java
+++ src/main/java/com/employeeProject/spring/employeeprojecttwo/entity/Employee.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 public class Employee {
 	
 	
-		protected Employee() {
+		public Employee() {
 			
 		}
 	
